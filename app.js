27a7364a2797e86a4493ceb6bcbd4a19637efb2f
Geet Sebastian <geetsebastian@Geets-MacBook-Pro.local>
2017-11-01 21:28:32 +1100

    bugs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/home', function(req, res){
+app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
